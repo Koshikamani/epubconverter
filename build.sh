ba7f98d422caa39830e563de3fb682d7cafc98bd
Koshikamani <turnipstack@gmail.com>
2025-03-11 02:41:58 +0530

    Added build script to fix missing dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
-apt-get update && apt-get install -y gobject-introspection libgirepository1.0-dev
+#!/usr/bin/env bash
+
+# Update package list
+apt-get update
+
+# Install system dependencies required for pygobject
+apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0
+
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
 
