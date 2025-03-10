96c7d52bc95fd07080fd7652466a0d5ea1af2fc4
Koshikamani <turnipstack@gmail.com>
2025-03-11 02:45:37 +0530

    Fix: Install gobject-introspection dependencies

--- build.sh
+++ build.sh
@@ -3,11 +3,19 @@
 # Update package list
 apt-get update
 
-# Install system dependencies required for pygobject
-apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0
+# Install system dependencies for pygobject
+apt-get install -y \
+  libgirepository1.0-dev \
+  gir1.2-gtk-3.0 \
+  gobject-introspection \
+  build-essential \
+  pkg-config \
+  libcairo2-dev \
+  libpango1.0-dev \
+  libgdk-pixbuf2.0-dev \
+  libgtk-3-dev
 
 # Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
 
